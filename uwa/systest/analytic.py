b4ea5674c721f4fcf4a5d8c6a0712da93e7e0842
PatrickSunter <patdevelop@gmail.com>
2010-04-16 15:15:37 +1000

    Split the systest.py code into:
    api.py: contain core system testing functionality
    analytic.py: Analytic SysTest sub-classes.

--- uwa/systest/analytic.py
+++ uwa/systest/analytic.py
@@ -1,42 +1,7 @@
 
-class SysTestRunner:
-
-    def __init__(self, sysTest):
-        # Nothing for the base class
-        pass
-        self.sysTest = sysTest
-    
-    def runTest(self):
-        # Generate a suite of models to run as part of the test
-        mSuite = sysTest.genSuite()
-
-        suiteResults = mSuite.runSuite()
-        suiteResults.writeAllXMLs(sysTest.outputPath)
-
-        testResult = sysTest.getStatus(suiteResults)
-        sysTest.writeXML(sysTest.outputPath)
-       
-
-class SysTest:
-    def __init__():
-        
-
-    def genSuite():
-        print "Error, base class"
-        assert 0
-
-    def getStatus( suiteResults ):
-        print "Error, base class"
-        
-    def writeXML( outputPath ):    
-        # Create the XML file, and standard tags
-        # Write standard stuff like descriptions, etc
-        print "Error, base class"
-
-    def writeXMLContents( outputPath ):
-        # Write the contents of this particular test
-
 import uwa.analysis
+
+# TODO: have a factory for these to register with, in the API?
 
 class AnalyticTest(SysTest):
     '''An Analytic System test.
