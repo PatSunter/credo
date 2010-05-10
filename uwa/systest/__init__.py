e0b4599ac36e0501f6722811de8e209668c685ac
PatrickSunter <patdevelop@gmail.com>
2010-05-10 15:21:28 +1000

    Added the AnalyticMultiResTest to the list of standard tests.

--- uwa/systest/__init__.py
+++ uwa/systest/__init__.py
@@ -5,4 +5,5 @@ from uwa.systest.systestrunner import SysTestRunner
 # Import all the standard tests so they're available
 from uwa.systest.restart import RestartTest
 from uwa.systest.analytic import AnalyticTest
+from uwa.systest.analyticMultiRes import AnalyticMultiResTest
 from uwa.systest.reference import ReferenceTest
