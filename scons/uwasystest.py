d4a7a711d9ccbfba228856ebea30d170aca18c69
PatrickSunter <patdevelop@gmail.com>
2010-07-15 12:37:08 +1000

    Moved the System test-controlling SCons commands into UWA, from PCU.

---
+++ scons/uwasystest.py
@@ -0,0 +1,61 @@
+from SCons.Script import *
+#from SCons.Builder import Builder
+
+class ToolUWASysTestWarning(SCons.Warnings.Warning):
+    pass
+
+SCons.Warnings.enableWarningClass(ToolUWASysTestWarning)
+
+def generate(env, **kw):
+    env.SetDefault(INTEGRATION_TARGET="check-integration")
+    env.SetDefault(CONVERGENCE_TARGET="check-convergence")
+    env.SetDefault(LOWRES_TARGET="check-lowres")
+
+    # Several of the tests also refer to targets defined initially in
+    #  PCU testing, in pcutest.py
+    def IntegrationTest(env, target, source, **kw):
+        script = File(source[0].split()[0]).srcnode().abspath
+        script_dir = os.path.dirname(script)
+        script = os.path.basename(script)
+        args = source[0].split()[1:]
+
+        runner = env.Action('-./' + script + ' ' + ' '.join(args), chdir=script_dir)
+        env.Alias(env["INTEGRATION_TARGET"], [], runner)
+        env.AlwaysBuild(env["INTEGRATION_TARGET"])
+        env.Alias(env['PCUALL_TARGET'], env['INTEGRATION_TARGET'])
+        return None
+
+
+    def LowResTest(env, target, source, **kw):
+        script = File(source[0].split()[0]).srcnode().abspath
+        script_dir = os.path.dirname(script)
+        script = os.path.basename(script)
+        args = source[0].split()[1:]
+
+        runner = env.Action('-./' + script + ' ' + ' '.join(args), chdir=script_dir)
+        env.Alias(env["LOWRES_TARGET"], [], runner)
+        env.AlwaysBuild(env["LOWRES_TARGET"])
+        env.Alias(env['PCUALL_TARGET'], env['LOWRES_TARGET'])
+        env.Alias(env['REGTEST_TARGET'], env['LOWRES_TARGET'])
+        return None
+
+    def ConvergenceTest(env, target, source, **kw):
+        script = File(source[0].split()[0]).srcnode().abspath
+        script_dir = os.path.dirname(script)
+        script = os.path.basename(script)
+        args = source[0].split()[1:]
+
+        runner = env.Action('-./' + script + ' ' + ' '.join(args), chdir=script_dir)
+        env.Alias(env["CONVERGENCE_TARGET"], [], runner)
+        env.AlwaysBuild(env["CONVERGENCE_TARGET"])
+        env.Alias(env['PCUALL_TARGET'], env['CONVERGENCE_TARGET'])
+        return None
+
+    env.Append(BUILDERS={"LowResTest":LowResTest, 
+        "IntegrationTest":IntegrationTest,
+        "ConvergenceTest":ConvergenceTest})
+
+def exists(env):
+    # Should probably have this search for the uwa
+    # libraries/source or something.
+    return True        
