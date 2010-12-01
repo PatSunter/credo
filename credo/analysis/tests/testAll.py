ab9f699be41e2d34f13a015c82ccdf9f8a7b6cec
Patrick Sunter <patdevelop@gmail.com>
2010-12-01 14:54:20 +1100

    Added a testAll.py in the analysis/tests dir.

---
+++ credo/analysis/tests/testAll.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+##  Copyright (C), 2010, Monash University
+##  Copyright (C), 2010, Victorian Partnership for Advanced Computing (VPAC)
+##  
+##  This file is part of the CREDO library.
+##  Developed as part of the Simulation, Analysis, Modelling program of 
+##  AuScope Limited, and funded by the Australian Federal Government's
+##  National Collaborative Research Infrastructure Strategy (NCRIS) program.
+##
+##  This library is free software; you can redistribute it and/or
+##  modify it under the terms of the GNU Lesser General Public
+##  License as published by the Free Software Foundation; either
+##  version 2.1 of the License, or (at your option) any later version.
+##
+##  This library is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+##  Lesser General Public License for more details.
+##
+##  You should have received a copy of the GNU Lesser General Public
+##  License along with this library; if not, write to the Free Software
+##  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+##  MA  02110-1301  USA
+
+import glob
+import unittest
+
+def suite():
+    #testMods = [fName.rstrip(".py") for fName in glob.glob("*suite.py")]
+    testMods = ['analysissuite', 'fieldssuite', 'imagessuite']
+    # TODO: others need fixing
+    alltests = unittest.TestSuite()
+    for module in map(__import__, testMods):
+        alltests.addTest(unittest.findTestCases(module))
+    return alltests
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
