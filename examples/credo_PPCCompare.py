a16f9e72e086f0efeb177a270ca373945d8b610c
Patrick Sunter <patdevelop@gmail.com>
2011-02-16 14:33:14 +1100

    Saving updated PPC Compare script in CREDO examples.

--- examples/credo_PPCCompare.py
+++ examples/credo_PPCCompare.py
@@ -11,16 +11,16 @@ stdRun = ModelRun("Arrhenius-normal",
     os.path.join('..','..', 'Underworld', 'InputFiles', 'Arrhenius.xml'),
     "output/arrBasic", simParams=defParams)
 ppcRun = ModelRun("Arrhenius-ppc", "Arrhenius.xml",
-    outputPath="output/arrPIC",
     basePath=os.path.join("Ppc_Testing","udw_inputfiles"),
+    outputPath="output/arrPIC",
     simParams=defParams)
 
 stdSuite = ModelSuite(os.path.join("output", "arrBasic"))
 ppcSuite = ModelSuite(os.path.join(os.getcwd(), "output", "arrPIC"))
 
 for ii in range(10):
-    stdRun.outputPath = os.path.join(stdSuite.outputPathBase, "%d" % ii)
-    ppcRun.outputPath = os.path.join(ppcSuite.outputPathBase, "%d" % ii)
+    stdRun.outputPath = os.path.join(stdSuite.outputPathBase, "%.5d" % ii)
+    ppcRun.outputPath = os.path.join(ppcSuite.outputPathBase, "%.5d" % ii)
     stdSuite.addRun(copy.deepcopy(stdRun))
     ppcSuite.addRun(copy.deepcopy(ppcRun))
 
